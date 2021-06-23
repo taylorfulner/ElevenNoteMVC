a1ec5144d09ace4e129640ae8ccd252ac5ec4fdf
Taylor <taylorfulner@gmail.com>
2021-06-23 16:39:26 -0400

    added Note Service

--- ElevenNote.Data/Note.cs
+++ ElevenNote.Data/Note.cs
@@ -13,7 +13,7 @@ namespace ElevenNote.Data
         public int NoteId { get; set; }
 
         [Required]
-        public Guid guid { get; set; }
+        public Guid OwnerId { get; set; }
 
         [Required]
         public string Title { get; set; }
---
+++ ElevenNote.Services/NoteService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class NoteService
+    {
+        private readonly Guid _userId;
+
+        public NoteService(Guid userId)
+        {
+            _userId = userId;
+        }
+    }
+}
