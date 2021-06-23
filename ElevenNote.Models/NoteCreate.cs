e6ef08ba1f7da2ef22ccad465d913d6621c2ab4f
Taylor <taylorfulner@gmail.com>
2021-06-23 16:20:26 -0400

    added note create

---
+++ ElevenNote.Models/NoteCreate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Please Enter at least 2 characters.")]
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+    }
+}
--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElevenNote.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,30 @@ using System.Web.Mvc;
 
 namespace ElevenNote.WebMVC.Controllers
 {
+    [Authorize]
     public class NoteController : Controller
     {
         // GET: Note
         public ActionResult Index()
         {
+            var model = new NoteListItem[0];
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
         }
     }
 }
